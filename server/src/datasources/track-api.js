0c7a0661cb6f89ba4a6ae85294c011937845d989
Helen L <htslin103@gmail.com>
2021-10-11 14:07:29 -0600

    Removed Mocks, added API datasource

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,17 +1,19 @@
-const { RESTDataSource } = require("apollo-datasource-rest");
+const { RESTDataSource } = require('apollo-datasource-rest');
 
-class TrackAPI extends RESTDataSource{
-    constructor(){
-        super();
-        this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com";
-    }
-    getTracksForHome(){
-        return this.get("tracks");
-    }
+class TrackAPI extends RESTDataSource {
+  constructor() {
+    super();
+    // the Catstronauts catalog is hosted on this server
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  }
 
-    getAuthors(authorId){
-        return this.get(`author/${authorId}`);
-    }
+  getTracksForHome() {
+    return this.get('tracks');
+  }
+
+  getAuthor(authorId) {
+    return this.get(`author/${authorId}`);
+  }
 }
 
-module.exports = TracksAPI;
+module.exports = TrackAPI;
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,16 +1,16 @@
 const resolvers = {
-    //add a Query key first
-    //returns an array of Tracks that will be used to
-    //populate the homepage grid of our web client
     Query: {
-        tracksForHome: (_, __, {dataSources},) => {
-            dataSources.trackAPI.getTracksForHome();             
-    }
-},
+      // returns an array of Tracks that will be used to populate the homepage grid of our web client
+      tracksForHome: (_, __, { dataSources }) => {
+        return dataSources.trackAPI.getTracksForHome();
+      },
+    },
     Track: {
-    author: ({authorID}, _, {dataSources}) => {
-            dataSources.trackAPI.getAuthor(authorID);
-        }
-    }
-};
-module.exports = resolvers;
+      author: ({ authorId }, _, { dataSources }) => {
+        return dataSources.trackAPI.getAuthor(authorId);
+      },
+    },
+  };
+  
+  module.exports = resolvers;
+  
